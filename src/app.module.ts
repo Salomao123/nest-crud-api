8bc7fdc72191a39ee850afff2432d331bd1cc645
Salomao do Nacimento Batista <salomao.batista@fornecedores.sicoob.com.br>
2022-02-14 20:54:53 -0300

    installing mongoose and creating entity and service from users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://salomao:zYGswUGSpDXiMLZD@cluster0.pbcz5.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
